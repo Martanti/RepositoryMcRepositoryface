a41b580dd01f2f1341eca9ee1822c83d449da633
Kreisis <kreisis4@gmail.com>
2017-07-19 15:11:44 +0300

    Read All tables from database
    About to start work on a backup project

--- ProjectyMcProjectface/AdventureWorksConnection/Program.cs
+++ ProjectyMcProjectface/AdventureWorksConnection/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +13,57 @@ namespace AdventureWorksConnection
     {
         static void Main(string[] args)
         {
+            SqlServerTypes.Utilities.LoadNativeAssemblies(AppDomain.CurrentDomain.BaseDirectory);
+            
+            ReadAllTablesFromDatabase(@"Data Source=.\SQLEXPRESS;Database=AdventureWorks2014;Integrated Security=True;");
+            
+
             Console.ReadLine();
+        }
+
+        public static void ReadAllTablesFromDatabase(string connectionString)
+        {
+            SqlConnection con = new SqlConnection(connectionString);
+            con.Open();
+
+            List<string> tables = GetTableList(con);
+            foreach(var table in tables)
+            {
+                Console.WriteLine(table);
+                PrintTableData(table, con);
+                Console.WriteLine();
+            }
+
+            con.Close();
+        }
+        public static void PrintTableData(string tableName, SqlConnection con)
+        {
+            DataTable dataTable = new DataTable();
+
+            string query = "select * from " + tableName;
+            SqlCommand command = new SqlCommand(query, con);
+            SqlDataAdapter da = new SqlDataAdapter(command);
+
+            da.Fill(dataTable);
+
+            da.Fill(dataTable);
+
+            for (int x = 0; x < dataTable.Rows.Count; x++)
+            {
+                string line = "";
+
+                for (int y = 0; y < dataTable.Columns.Count; y++)
+                {
+                    line = line + dataTable.Columns[y].ColumnName + ": " + dataTable.Rows[x].ItemArray[y].ToString() + "; ";
+                }
+                Console.WriteLine(line);
+            }
+            da.Dispose();
+        }
+        public static List<string> GetTableList(SqlConnection connection)
+        {
+            List<string> tables = connection.GetSchema("Tables").AsEnumerable().Select(S => S[1].ToString() + "." + S[2].ToString()).ToList();
+            return tables;
         }
         
     }
