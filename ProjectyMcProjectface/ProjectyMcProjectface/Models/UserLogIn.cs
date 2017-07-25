34bde6bde5e426a69a1e53f289c7439c4cf209e2
Martynas Antipenkovas <martynas.antipenkovas@hotmail.lt>
2017-07-25 13:49:56 +0300

    reworking login  page

---
+++ ProjectyMcProjectface/ProjectyMcProjectface/Models/UserLogIn.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjectyMcProjectface.Models
+{
+    public class UserLogIn
+    {
+        [DisplayName("User Name")]
+        [Required(ErrorMessage = "This field is required to be filled")]
+        public string UserName { get; set; }
+
+        [DataType(DataType.Password)]
+        [DisplayName("Password")]
+        [Required(ErrorMessage = "This field is required to be filled")]
+        public string Password { get; set; }
+
+        public string LoginErrorMessage { get; set; }
+    }
+}
