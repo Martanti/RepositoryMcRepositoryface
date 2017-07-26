bea6b83a2b128674ae4b33837cb7250c1003366f
Martynas Antipenkovas <martynas.antipenkovas@hotmail.lt>
2017-07-26 11:46:54 +0300

    GitIgnore update

--- ProjectyMcProjectface/ProjectyMcProjectface/App_Start/RouteConfig.cs
+++ ProjectyMcProjectface/ProjectyMcProjectface/App_Start/RouteConfig.cs
@@ -12,13 +12,6 @@ namespace ProjectyMcProjectface
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-            /*
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Login", action = "Index", id = UrlParameter.Optional }
-            );
-            */
 
             routes.MapRoute(
                 name: "LoginPage",
