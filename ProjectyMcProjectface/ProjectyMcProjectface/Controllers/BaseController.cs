d4cbbeebe37e44a9c95dad41b13b149dbebdd165
Kreisis <kreisis4@gmail.com>
2017-07-28 13:47:48 +0300

    Added base controller with logging functionality

---
+++ ProjectyMcProjectface/ProjectyMcProjectface/Controllers/BaseController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+using log4net;
+using System.Diagnostics;
+
+namespace ProjectyMcProjectface.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected override void OnException(ExceptionContext filterContext)
+        {
+            ILog log = log4net.LogManager.GetLogger(typeof(ErrorController));
+
+            var st = new StackTrace(filterContext.Exception, true);
+            log.Error(filterContext.Exception.Message);
+            /*base.OnException(filterContext);*/
+        }
+    }
+}
